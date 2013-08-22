a20b3565d189e8dec8eaec636c74010d888903c6
ThatGuy <niels.groot.obbink@ddvtech.com>
2013-08-22 15:50:44 +0200

    modified error message texts

--- lib/classes/error.class.php
+++ lib/classes/error.class.php
@@ -39,7 +39,7 @@ class Error {
 		(
 			'statuscode' => '404',
 			'title' => 'Page not found',
-			'description' => 'We couldn\'t find the page you were looking for. Sorry! :('
+			'description' => 'We couldn\'t find the page you were looking for.'
 		),
 
 		'500' => array
