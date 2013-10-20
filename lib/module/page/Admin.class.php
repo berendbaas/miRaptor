7a815bc9c25d2c136c3e356d1b2c9d601bcdce7a
Berend <berendbaas@gmail.com>
2013-10-20 12:04:26 +0200

    Page Admin Update
    Work in progress.

--- lib/module/page/Admin.class.php
+++ lib/module/page/Admin.class.php
@@ -9,8 +9,67 @@ namespace lib\module\page;
  */
 class Admin extends \lib\core\AbstractAdmin {
 	public function run() {
-		$this->result = 'TODO page admin';
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			$request = 'POST';
+		} else {
+			$request = 'GET';
+		}
+
+		if (!isset($_GET['action'])) {
+			$action = 'Overview';
+		} else {
+			$action = $_GET['action'];
+		}
+
+		$function = $request . $action;
+		$this->$function();
 	}
+
+	private function getOverview()
+	{
+		$this->result .= <<<HTML
+<h1>Pages</h1>
+<table>
+<tr>
+	<th>ID</th>
+	<th>Name</th>
+	<th>Uri</th>
+	<th>Edit</th>
+	<th>Remove</th>
+	<th></th>
+</tr>
+HTML;
+		$this->pdbc->query('SELECT * FROM module_page 
+								INNER JOIN router 
+									ON router.id = module_page.rid 
+								INNER JOIN template 
+									ON template.id = router.tid');
+
+		$pages = $this->pdbc->fetchAll();
+		foreach ($pages as $no => $page) {
+			$this->result .= <<<HTML 
+<tr>
+	<td>{$page['id']}</td>
+	<td>{$page['name']}</td>
+	<td>{$page['uri']}</td>
+	<td></td>
+	<td></td>
+
+</tr>
+HTML;
+		}
+	}
+
+	private function getEdit()
+	{
+
+	}
+
+	private function postEdit()
+	{
+
+	}
+
 }
 
 ?>
