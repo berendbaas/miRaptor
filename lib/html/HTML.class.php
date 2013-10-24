f9ff144ce7500561277fb9b2c70b86f2bc35a22b
Michael Webbers <info@miwebb.com>
2013-10-25 00:51:52 +0200

    Added the element function, a self-closing feature & comments

--- lib/html/HTML.class.php
+++ lib/html/HTML.class.php
@@ -8,17 +8,43 @@ namespace lib\html;
  * @version 1.0
  */
 class HTML {
-	public static function tagStart($tag, array() $attributes) {
+	/**
+	 * Returns an HTML element with the given tag, attributes & content.
+	 *
+	 * @param  string $tag
+	 * @param  array  $attributes
+	 * @parem  string $content
+	 * @return string an HTML element with the given tag, attributes & content.
+	 */
+	public static function element($tag, array $attributes = array(), $content = '') {
+		return self::tagOpen($tag, $attributes) . $content . self::tagClose($tag);
+	}
+
+	/**
+	 * Returns an HTML opening / start tag with the given tag, attributes. You can also make the HTML tag self closing.
+	 *
+	 * @param  string  $tag
+	 * @param  array   $attributes
+	 * @param  boolean $closing
+	 * @return string  an HTML start tag with the given tag, attributes. You can also make the HTML tag self closing.
+	 */
+	public static function tagOpen($tag, array $attributes = array(), $closing = FALSE) {
 		$result = '<' . $tag;
 
-		foreach($attributs as $key => $value) {
+		foreach($attributes as $key => $value) {
 			$result .= ' ' . $key . '="' . $value . '"';
 		}
 
-		return $result . '>';
+		return $result . ($closing ? '/>' : '>');
 	}
 
-	public static function tagEnd($tag) {
+	/**
+	 * Returns an HTML closing / end tag with the given tag.
+	 *
+	 * @param  string $tag
+	 * @return string an HTML end tag with the given $tag.
+	 */
+	public static function tagClose($tag) {
 		return '</' . $tag . '>';
 	}
 }
