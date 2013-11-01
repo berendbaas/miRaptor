dee6a74be9fc62bee0cf13606d487b5ca5da3d15
Michael Webbers <info@miwebb.com>
2013-11-01 10:53:52 +0100

    Improved performance & class dependencies

--- lib/html/HTMLList.class.php
+++ lib/html/HTMLList.class.php
@@ -8,20 +8,17 @@ namespace lib\html;
  * @version 1.0
  */
 class HTMLList {
-	private $listType;
-	private $list;
+	private $ordered;
+	private $result;
 
 	/**
 	 * Construct an HTML list with the given items & order.
 	 *
-	 * @param array   $items = array()
 	 * @param boolean $ordered = FALSE
 	 */
-	public function __construct(array $items = array(), $ordered = FALSE) {
-		$this->listType = $ordered ? 'ol' : 'ul';
-		$this->list = PHP_EOL;
-
-		$this->addItems($items);
+	public function __construct($ordered = FALSE) {
+		$this->ordered = $ordered;
+		$this->result = ($this->ordered ? '<ol>' : '<ul>') . PHP_EOL;
 	}
 
 	/**
@@ -30,21 +27,21 @@ class HTMLList {
 	 * @return string the string representation of the HTML List object.
 	 */
 	public function __toString() {
-		return HTML::element($this->listType, array(), $this->list) . PHP_EOL;
+		return $this->result . ($this->ordered ? '</ol>' : '</ul>') . PHP_EOL;
 	}
 
 	/**
-	 * Add the given item at the end of the HTML list.
+	 * Add the given item to the HTML list.
 	 *
 	 * @param string $item
 	 * @return void
 	 */
 	public function addItem($item) {
-		$this->list .= HTML::element('li', array(), $item) . PHP_EOL;
+		$this->result .= '<li>' . $item . '</li>' . PHP_EOL;
 	}
 
 	/**
-	 * Add the given items at the end of the HTML list.
+	 * Add the given items to the HTML list.
 	 *
 	 * @param array $items
 	 * @return void
