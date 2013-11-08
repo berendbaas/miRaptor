888169086c9e53a4e0069750e3b681c7cd05db4a
Michael Webbers <info@miwebb.com>
2013-11-08 22:43:45 +0100

    Breadcrumb: Small update

--- lib/module/breadcrumb/Module.class.php
+++ lib/module/breadcrumb/Module.class.php
@@ -8,26 +8,47 @@ namespace lib\module\breadcrumb;
  * @version 1.0
  */
 class Module extends \lib\core\AbstractModule {
+	public function __construct(\lib\pdbc\PDBC $pdbc, \lib\core\URL $url, $routerID, array $arguments) {
+			parent::__construct($pdbc, $url, $routerID, $arguments);
+			$this->isStatic = TRUE;
+	}
+
 	public function run() {
-		$this->result = <<<HTML
-<ul>{$this->parseBreadcrumb($this->routerID)}
-</ul>
-HTML;
+		$this->result = $this->getBreadcrumb($this->routerID);
+
 	}
 
 	/**
+	 * Returns the breadcrumbs starting at the given ID.
 	 *
+	 * @param  int                       $id
+	 * @return String                    the breadcrumbs starting at the given ID.
+	 * @throws \lib\pdbc\PDBCException   if the given query can't be executed.
 	 */
-	private function parseBreadcrumb($id) {
+	private function getBreadcrumb($id) {
+		$list = new \lib\html\HTMLList();
+
+		while($breadcrumb = $this->fetchBreadcrumb($id)) {
+			$list->addItem('<a href="' . $breadcrumb['uri'] . '">' . $breadcrumb['name'] .'</a>');
+			$id = $breadcrumb['id'];
+		}
+
+		return $list->__toString();
+	}
+
+	/**
+	 * Returns an array with with the pid, name & uri of breadcrumb with the given ID.
+	 *
+	 * @param  int                       $id
+	 * @return array                     an array with with the pid, name & uri of breadcrumb with the given ID.
+	 * @throws \lib\pdbc\PDBCException   if the given query can't be executed.
+	 */
+	private function fetchBreadcrumb($id) {
 		$this->pdbc->query('SELECT `pid`,`name`,`uri`
 		                    FROM `router`
 		                    WHERE `id` = "' . $this->pdbc->quote($id) . '"');
 
-		$breadcrumb = $this->pdbc->fetch();
-
-		return !$breadcrumb ? '' : $this->parseBreadcrumb($breadcrumb['pid']) . PHP_EOL . <<<HTML
-<li><a href="{$breadcrumb['uri']}">{$breadcrumb['name']}</a></li>
-HTML;
+		return $this->pdbc->fetch();
 	}
 }
 
