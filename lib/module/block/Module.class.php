3ce11d83ee3e6b47e0c07ceaa511df7c24c29c39
Michael Webbers <info@miwebb.com>
2013-11-08 22:43:53 +0100

    Block: Small update

--- lib/module/block/Module.class.php
+++ lib/module/block/Module.class.php
@@ -8,36 +8,69 @@ namespace lib\module\block;
  * @version 1.0
  */
 class Module extends \lib\core\AbstractModule {
+	const DEFAULT_GROUP = '';
+
+	public function __construct(\lib\pdbc\PDBC $pdbc, \lib\core\URL $url, $routerID, array $arguments) {
+			parent::__construct($pdbc, $url, $routerID, $arguments);
+			$this->isParsable = TRUE;
+			$this->isStatic = TRUE;
+	}
+
 	public function run() {
-		$this->result = $this->parseBlock($this->parseGet());
+		$this->result = $this->getBlock($this->parseName(), $this->parseGroup());
 	}
 
 	/**
+	 * Returns the name argument, if one is given.
 	 *
+	 * @return string                    the name argument, if one is given.
+	 * @throws \lib\core\ModuleException if the name argument isn't given.
 	 */
-	private function parseGet() {
-		if(isset($this->arguments['get'])) {
-			return $this->arguments['get'];
+	private function parseName() {
+		if(isset($this->arguments['name'])) {
+			return $this->arguments['name'];
 		}
 
-		throw new \Exception('get="" required.');
+		throw new \lib\core\ModuleException('name="" required.');
 	}
 
 	/**
+	 * Returns the group argument or the default argument, if none is given.
 	 *
+	 * @return string the group argument or the default argument, if none is given.
 	 */
-	private function parseBlock($get) {
+	private function parseGroup() {
+		if(isset($this->arguments['group'])) {
+			return $this->arguments['group'];
+		}
+
+		return self::DEFAULT_GROUP;
+	}
+
+	/**
+	 * Returns the block with the given name and group.
+	 *
+	 * @param  string                    $name
+	 * @param  string                    $group = self::DEFAULT_GROUP
+	 * @return string                    the block with the given name and group.
+	 * @throws \lib\core\ModuleException if there is no block for the given name & group.
+	 * @throws \lib\pdbc\PDBCException   if the given query can't be executed.
+	 */
+	private function getBlock($name, $group) {
 		$this->pdbc->query('SELECT `content`
 		                    FROM `module_block`
-		                    WHERE `name` = "' . $this->pdbc->quote($get) . '"');
+		                    WHERE `name` = "' . $this->pdbc->quote($name) . '"
+		                    AND `id_group` ' . ($group == self::DEFAULT_GROUP ? 'is NULL' : '= (SELECT `id`
+		                                                                                        FROM `group`
+		                                                                                        WHERE `name` = "' . $this->pdbc->quote($group) . '")'));
 
-		$site = $this->pdbc->fetch();
+		$block = $this->pdbc->fetch();
 
-		if(!$site) {
-			throw new \Exception('get="' . $get . '" does not exists.');
+		if(!$block) {
+			throw new \lib\core\ModuleException('name="' . $name . '" does not exists.');
 		}
 
-		return end($site);
+		return end($block);
 	}
 }
 
