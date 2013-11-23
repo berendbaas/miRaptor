fea1173d0257d6aa63551f888686ba576e83dcaf
Michael Webbers <info@miwebb.com>
2013-11-23 20:47:55 +0100

    Update page admin (part 1)

--- lib/module/page/Admin.class.php
+++ lib/module/page/Admin.class.php
@@ -8,262 +8,273 @@ namespace lib\module\page;
  * @version 1.0
  */
 class Admin extends \lib\core\AbstractAdmin {
-	public function run() {
-		$request = ($_SERVER['REQUEST_METHOD'] == 'POST') ? 'POST' : 'GET';
-		$action = !isset($_GET['action']) ? 'Overview' : $_GET['action'];
+	const ACTION_NEW = 'new';
+	const ACTION_EDIT = 'edit';
+	const ACTION_REMOVE = 'remove';
 
-		$function = $request . $action;
-		if (method_exists($this, $function)) {
-			$this->$function();
-		} else {
-			throw new \Exception('Action is not implemented', 501);
+	public function run() {
+		$this->result = isset($_GET['action'], $_GET['tid']) ? $this->handleAction($_GET['action'], $_GET['tid']) : $this->getOverview();
+	}
+
+	/**
+	 *
+	 */
+	private function handleAction($action, $id) {
+		switch($action) {
+			case self::ACTION_NEW:
+				return $this->getNew($id, ($_SERVER['REQUEST_METHOD'] == 'POST' ? $this->postNew($id) : ''));
+			break;
+			case self::ACTION_EDIT:
+				return $this->getEdit($id, ($_SERVER['REQUEST_METHOD'] == 'POST' ? $this->postEdit($id) : ''));
+			break;
+			case self::ACTION_REMOVE:
+				return $this->getRemove($id, ($_SERVER['REQUEST_METHOD'] == 'POST' ? $this->postRemove($id) : ''));
+			break;
+			default:
+				// TODO
+			break;
 		}
 	}
 
-	private function getOverview()
-	{
-		$base = $this->url->getURLDirectory();
-		$id = $_GET['id'];
-	
-		$this->result .= <<<HTML
-<h1>Page</h1>
-<table>
-<tr>
-	<th>ID</th>
-	<th>Name</th>
-	<th>Uri</th>
-	<th>Description</th>
-	<th>Template</th>
-	<th>Edit</th>
-	<th>Remove</th>
-</tr>
-HTML;
-		$this->pdbc->query('SELECT p.id as id, p.description as description, r.name as name, r.uri as uri, t.name as template FROM module_page AS p
-								INNER JOIN router AS r
-									ON r.id = p.rid 
-								INNER JOIN template AS t
-									ON t.id = r.tid');
+	/**
+	 *
+	 */
+	private function postNew($id) {
+		$this->pdbc->query('INSERT IGNORE INTO `router` (`pid`,`index`,`name`,`uri`)
+		                    SELECT CASE COUNT(`r1`.`id`) WHEN 0 THEN NULL ELSE `r1`.`id` END,
+		                           MAX(`r2`.`index`) + 1,
+		                           "' . $this->pdbc->quote($_POST['name']) . '",
+		                           CONCAT(CASE COUNT(`r1`.`id`)
+		                                  WHEN 0 THEN "/"
+		                                  ELSE CASE `r1`.`root`
+		                                       WHEN 1 THEN CONCAT("/", LOWER(`r1`.`name`) ,"/")
+		                                       WHEN 0 THEN `r1`.`uri`
+		                                       END
+		                                  END,
+		                                  "' . strtolower($this->pdbc->quote($_POST['name'])) . '/")
+		                    FROM `router` AS `r1`, `router` AS `r2`
+		                    WHERE `r1`.`name` = "' . $this->pdbc->quote($_POST['parent']) . '"');
+
+		if(!$this->pdbc->rowCount()) {
+			return '<p class="msg-error">This page already exists. Please try again.</p>';
+		}
+
+		$this->pdbc->query('INSERT INTO `module_page` (`id_router`,`id_template`,`content`)
+		                    SELECT "' . $this->pdbc->insertID() . '",
+		                           `module_template`.`id`,
+		                           "' . $this->pdbc->quote($_POST['content']) . '"
+		                    FROM `module_template`
+		                    WHERE `module_template`.`name` = "' . $_POST['template'] . '"');
+
+		throw new \lib\core\StatusCodeException($this->url->getURLPath() . '?id=' . $_GET['id'] .  '&module=page', \lib\core\StatusCodeException::REDIRECTION_SEE_OTHER);
+	}
+
+	/**
+	 *
+	 */
+	private function getNew($id, $message = '') {
+		// Parents
+		$this->pdbc->query('SELECT `name` FROM `router`');
+
+		$parents = array('');
+
+		while($parent = $this->pdbc->fetch()) {
+			$parents[] = $parent['name'];
+		}
+
+		// Templates
+		$this->pdbc->query('SELECT `name` FROM `module_template`');
+
+		$templates = array();
+
+		while($template = $this->pdbc->fetch()) {
+			$templates[] = $template['name'];
+		}
+
+		// Form
+		$form = new \lib\html\HTMLFormStacked();
+
+		$form->addInput('Name', array(
+			'type' => 'text',
+			'id' => 'form-name',
+			'name' => 'name',
+			'placeholder' => 'Name'
+		));
+
+		$form->addSelect('Parent', $parents, array(
+			'id' => 'form-parent',
+			'name' => 'parent'
+		));
+
+		$form->addTextarea('Content', '', array(
+			'id' => 'form-content',
+			'name' => 'content',
+			'placeholder' => 'Content'
+		));
+
+		$form->addSelect('Template', $templates, array(
+			'id' => 'form-template',
+			'name' => 'template'
+		));
+
+		$form->addContent('<a href="' . $this->url->getPath() . '?id=' . $_GET['id'] .  '&amp;module=page' . '"><button type="button">Back</button></a>');
+
+		$form->addButton('Submit', array(
+			'type' => 'submit'
+		));
+
+		return '<h2 class="icon icon-module-page">New page</h2>' . $message . $form->__toString();
+
+	}
+
+	/**
+	 *
+	 */
+	private function postEdit($id) {
+		$this->pdbc->query('UPDATE `module_page`
+		                    SET `content` = "'. $this->pdbc->quote($_POST['content']) .'"
+		                    WHERE `id` = "'. $this->pdbc->quote($id) .'"');
+
+		throw new \lib\core\StatusCodeException($this->url->getURLPath() . '?id=' . $_GET['id'] .  '&module=page', \lib\core\StatusCodeException::REDIRECTION_SEE_OTHER);
+	}
+
+	/**
+	 *
+	 */
+	private function getEdit($id, $message = '') {
+		// Parents
+		$this->pdbc->query('SELECT `name` FROM `router`');
+
+		$parents = array('');
+
+		while($parent = $this->pdbc->fetch()) {
+			$parents[] = $parent['name'];
+		}
+
+		// Templates
+		$this->pdbc->query('SELECT `name` FROM `module_template`');
+
+		$templates = array();
+
+		while($template = $this->pdbc->fetch()) {
+			$templates[] = $template['name'];
+		}
+
+		// Edit
+		$this->pdbc->query('SELECT `module_page`.`content`, `router`.`name`, `router`.`uri`
+		                    FROM `module_page`
+		                    LEFT JOIN (SELECT `id`,`name`,`uri`
+		                               FROM `router`) AS `router`
+		                    ON `module_page`.`id_router` = `router`.`id`
+		                    WHERE `module_page`.`id` = "' . $this->pdbc->quote($id) . '"');
+
+		$page = $this->pdbc->fetch();
+
+		// Form
+		$form = new \lib\html\HTMLFormStacked();
+
+		$form->addInput('Name', array(
+			'type' => 'text',
+			'id' => 'form-name',
+			'name' => 'name',
+			'placeholder' => 'Name',
+			'value' => $page['name']
+		));
+
+		$form->addSelect('Parent', $parents, array(
+			'id' => 'form-parent',
+			'name' => 'parent'
+		));
+
+		$form->addInput('Uri', array(
+			'type' => 'text',
+			'id' => 'form-uri',
+			'name' => 'uri',
+			'placeholder' => 'Uri',
+			'value' => $page['uri'],
+			'disabled' => 'disabled'
+		));
+
+		$form->addTextarea('Content', $page['content'], array(
+			'id' => 'form-content',
+			'name' => 'content',
+			'placeholder' => 'Content'
+		));
+
+		$form->addSelect('Template', $templates, array(
+			'id' => 'form-template',
+			'name' => 'template'
+		));
+
+		$form->addContent('<a href="' . $this->url->getPath() . '?id=' . $_GET['id'] .  '&amp;module=page' . '"><button type="button">Back</button></a>');
+
+		$form->addButton('Submit', array(
+			'type' => 'submit'
+		));
+
+		return '<h2 class="icon icon-module-page">Edit page</h2>' . $message . $form->__toString();
+	}
+
+	/**
+	 *
+	 */
+	private function postRemove($id) {
+		$this->pdbc->query('DELETE FROM `router` 
+		                    WHERE `id` = (SELECT `id_router`
+		                                  FROM `module_page`
+						  WHERE `id` = "' . $this->pdbc->quote($id) . '")');
+
+		if(!$this->pdbc->rowCount()) {
+
+		}
+
+		throw new \lib\core\StatusCodeException($this->url->getURLPath() . '?id=' . $_GET['id'] .  '&module=page', \lib\core\StatusCodeException::REDIRECTION_SEE_OTHER);
+	}
+
+	/**
+	 *
+	 */
+	private function getRemove($id, $message = '') {
+		$form = new \lib\html\HTMLFormStacked();
+
+		$form->addContent('<label>Are you sure you want to remove this page? This action can\'t be undone!</label>');
+
+		$form->addContent('<a href="' . $this->url->getPath() . '?id=' . $_GET['id'] .  '&amp;module=page' . '"><button type="button">No</button></a>');
+
+		$form->addButton('Yes', array(
+			'type' => 'submit'
+		));
+
+		return '<h2 class="icon icon-module-page">Remove page</h2>' . $message . $form->__toString();
+	}
+
+	/**
+	 *
+	 */
+	private function getOverview() {
+		$this->pdbc->query('SELECT `module_page`.`id`, `router`.`name`, `router`.`uri`
+		                    FROM `module_page`
+		                    LEFT JOIN (SELECT `id`,`name`,`uri`
+		                               FROM `router`) AS `router`
+		                    ON `module_page`.`id_router` = `router`.`id`');
 
 		$pages = $this->pdbc->fetchAll();
-		foreach ($pages as $no => $page) {
-			$this->result .= <<<HTML
-<tr>
-	<td>{$page['id']}</td>
-	<td>{$page['name']}</td>
-	<td>{$page['uri']}</td>
-	<td>{$page['description']}</td>
-	<td>{$page['template']}</td>
-	<td><a href="{$base}site?id={$id}&amp;module=page&amp;action=edit&amp;pid={$page['id']}" class="edititem" alt="Edit item">Edit</a></td>
-	<td><a href="{$base}site?id={$id}&amp;module=page&amp;action=remove&amp;pid={$page['id']}" class="removeitem" alt="Remove item">Remove</a></td>
-</tr>
-HTML;
-		}
-		$this->result .= <<<HTML
-</table>
-<a href="{$base}site?id={$id}&amp;module=page&amp;action=new">New Page</a>
-HTML;
-	}
 
-	private function getEdit(array $fields = array())
-	{
-		$base = $this->url->getURLDirectory();
-		$id = $_GET['id'];
-		$this->pdbc->query('SELECT p.id as id, 
-								p.description as description, 
-								r.name as name, 
-								r.uri as uri, 
-								t.name as template,
-								t.id as templateID,
-								p.content as content
-							FROM module_page AS p
-								INNER JOIN router AS r
-									ON r.id = p.rid 
-								INNER JOIN template AS t
-									ON t.id = r.tid
-							WHERE p.id = "'. $this->pdbc->quote($_GET['pid']) .'"');
-		$item = $this->pdbc->fetch();
+		$table = new \lib\html\HTMLTable();
+		$table->addHeaderRow(array('#','Name','Uri','Edit','Remove'));
 
-		$name = isset($fields['name']) ? $fields['name'] : $item['name'];
-		$content = isset($fields['content']) ? $fields['content'] : $item['content'];
-		$this->result .= <<<HTML
-<h1>Edit Page</h1>
-<form action="" method="POST">
-	<label for="name">Name</label>
-	<input type="text" name="name" value="{$name}" />
-	<label for="template">Template</label>
-	{$fields['template']}
-	<select name="template">
-HTML;
-		$this->pdbc->query('SELECT * FROM template');
-		foreach ($this->pdbc->fetchAll() as $no => $template) {
-			if ($template['id'] == $item['templateID']) {
-				$this->result .= <<<HTML
-<option value="{$template['id']}" selected="selected">* {$template['name']}</option>
-HTML;
-			}
-			else {
-				$this->result .= <<<HTML
-<option value="{$template['id']}">{$template['name']}</option>
-HTML;
-			}
-		}
-		$this->result .= <<<HTML
-	</select>
-	<label for="content">Content</label>
-	<textarea name="content">{$content}</textarea>
-
-	<a href="{$base}site?id={$id}&amp;module=page">Back</a>
-	<input type="submit">
-</form>
-HTML;
-	}
-
-	private function postEdit()
-	{
-		if(!$this->templateIDExists($_POST['template']))
-		{
-			$this->result .= <<<HTML
-<p class="error">Failed to update page</p>
-HTML;
-			$this->getEdit(array(
-					'content' => $_POST['content'],
-					'name' => $_POST['name'],
-					'template' => $_POST['template']
-				));
-			return;
-		}
-		$this->pdbc->query('UPDATE module_page as p
-								JOIN router AS r
-									ON r.id = p.rid
-								JOIN template AS t
-									ON t.id = r.tid
-								SET p.content = "'. $this->pdbc->quote($_POST['content']) .'",
-									r.name = "'. $this->pdbc->quote($_POST['name']) .'",
-									r.tid = '. $this->pdbc->quote($_POST['template']) .'
-								WHERE p.id = '. $this->pdbc->quote($_GET['pid']));
-		if ($this->pdbc->rowCount() == 0) {
-			$this->result .= <<<HTML
-<p class="error">Failed to update page.</p>
-HTML;
+		foreach($pages as $test => $page) {
+			$table->openRow();
+			$table->addColumn($page['id']);
+			$table->addColumn($page['name']);
+			$table->addColumn($page['uri']);
+			$table->addColumn('<a class="icon icon-edit" href="' . $this->url->getPath() . '?id=' . $_GET['id'] . '&amp;module=page&amp;action=' . self::ACTION_EDIT . '&amp;tid=' . $page['id'] . '"></a>');
+			$table->addColumn('<a class="icon icon-remove" href="' . $this->url->getPath() . '?id=' . $_GET['id'] . '&amp;module=page&amp;action=' . self::ACTION_REMOVE . '&amp;tid=' . $page['id'] . '"></a>');
+			$table->closeRow();
 		}
 
-			$this->getEdit(array(
-					'content' => $_POST['content'],
-					'name' => $_POST['name'],
-					'template' => $_POST['template']
-			));
+		return '<h2 class="icon icon-module-page">Page</h2>' . $table . '<p><a class="icon icon-new" href="' . $this->url->getPath() . '?id=' . $_GET['id'] . '&amp;module=page&amp;action=' . self::ACTION_NEW . '&amp;tid=0">New page</a></p>';
 	}
-
-	private function getNew(array $fields = array())
-	{
-		$base = $this->url->getURLDirectory();
-		$id = $_GET['id'];
-
-		$name = isset($fields['name']) ? $fields['name'] : "";
-		$template = isset($fields['template']) ? $fields['template'] : "";
-		$uri = isset($fields['uri']) ? $fields['uri'] : "";
-		$content = isset($fields['content']) ? $fields['content'] : "";
-
-		$this->result .= <<<HTML
-<h1>New Page</h1>
-<form action="" method="POST">
-	<label for="name">Name</label>
-	<input type="text" name="name" value="{$fields['name']}"/>
-	<label for="template">Template</label>
-	<select name="template" value="{$template}">
-HTML;
-		$this->pdbc->query('SELECT * FROM template');
-		foreach ($this->pdbc->fetchAll() as $no => $template) {
-			$this->result .= <<<HTML
-	<option value="{$template['id']}">{$template['name']}</opion>
-HTML;
-		}
-
-		$this->result .= <<<HTML
-	</select>
-	<label for="content">Code</label>
-	<textarea name="content">{$content}</textarea>
-
-	<input type="submit" />
-	<a href="{$base}site?id={$id}&amp;module=page">Back</a>
-</form>
-HTML;
-	}
-
-	private function postNew()
-	{
-		if ($this->templateIDExists($_POST['template'])) {
-			$this->pdbc->query('INSERT INTO router (pid, name, uri, tid) 
-								VALUES (
-									0,
-									"'. $this->pdbc->quote($_POST['name']) .'",
-									"/'. $this->pdbc->quote(strtolower($_POST['name'])) .'/",
-									'. $this->pdbc->quote($_POST['template']) .'
-									 )');
-
-			$this->pdbc->query('SELECT LAST_INSERT_ID() as id FROM router');
-			$rid = $this->pdbc->fetch()['id'];
-
-			$this->pdbc->query('INSERT INTO module_page (rid, description, content)
-								VALUES (
-									'. $rid .',
-									"Description",
-									"'. $this->pdbc->quote($_POST['content']) .'"
-									)');
-		}
-		else {
-			$this->result .= <<<HTML
-<p class="error">There was an error adding your page</p>
-HTML;
-			$this->getNew(array(
-				'name' => $_POST['name'],
-				'content' => $_POST['content'],
-				'template' => $_POST['template']
-				));
-		}
-		
-		$id = $_GET['id'];
-		$base = $this->url->getURLDirectory();
-		throw new \Exception($base . "site?id={$id}&module=page", 301);
-	}
-
-	private function getRemove()
-	{
-		$base = $this->url->getURLDirectory();
-		$id = $_GET['id'];
-		$this->pdbc->query('SELECT * FROM module_page 
-							JOIN router 
-								ON router.id = module_page.rid 
-							WHERE module_page.id = '. $this->pdbc->quote($_GET['pid']));
-		$page = $this->pdbc->fetch();
-		$this->result .= <<<HTML
-<h1>Removal notice</h1>
-<p>You are about to remove the page "{$page['name']}"</p>
-<p>Are you sure you want to remove this page? </p>
-<form action="" method="POST">
-<a href="{$base}site?id={$id}&amp;module=page">Back</a>
-<input type="submit" value="Remove" />
-</form>
-HTML;
-	}
-
-	private function postRemove()
-	{
-		$this->pdbc->query('DELETE FROM module_page 
-							USING module_page
-							JOIN router
-								ON router.id = module_page.rid
-							WHERE module_page.id = ' . $this->pdbc->quote($_GET['pid']));
-		$base =$this->url->getURLDirectory();
-		throw new \Exception($base . "site?id={$id}&module=page", 301);
-	}
-
-	private function templateIDExists($id)
-	{
-		$this->pdbc->query('SELECT id from template where id = "'. $this->pdbc->quote($id) .'"');
-		return $this->pdbc->rowCount() > 0;
-	}
-
 }
 
 ?>
