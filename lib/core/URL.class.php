282dd0a85377372203a0d7b65e92e494ada9540d
Michael Webbers <info@miwebb.com>
2014-01-05 15:28:11 +0100

    Update URL class

--- lib/core/URL.class.php
+++ lib/core/URL.class.php
@@ -9,40 +9,39 @@ namespace lib\core;
  */
 class URL {
 	const DELIMITER_SCHEME = '://';
+	const DELIMITER_SEGMENT = '/';
 	const DELIMITER_QUERY = '?';
+	const DELIMITER_QUERY_PAIR = '&';
+	const DELIMITER_QUERY_KEY_VALUE = '=';
+	const DELIMITER_FRAGMENT = '#';
 
 	private $scheme;
 	private $host;
-	private $directory;
-	private $file;
+	private $path;
 	private $query;
+	private $fragment;
 
 	/**
-	 * Construct a URL object with the given scheme, host & URI.
+	 * Construct a URL object with the given URL.
 	 *
-	 * @param string $scheme
-	 * @param string $host
-	 * @param string $uri
+	 * @param string $url
 	 */
-	public function __construct($scheme, $host, $uri) {
-		// Set scheme & host.
-		$this->scheme = $scheme;
-		$this->host = $host;
+	public function __construct($url) {
+		// Parse URL components
+		$component = parse_url($url);
 
-		// Split URI in directory, file and query.
-		$uri = explode(self::DELIMITER_QUERY, $uri, 2);
-		$pos = strrpos($uri[0], '/') + 1;
-
-		// Set directory, file & query.
-		$this->directory = substr($uri[0], 0, $pos);
-		$this->file = strlen($uri[0]) > $pos ? substr($uri[0], $pos) : '';
-		$this->query = (isset($uri[1]) ? $uri[1] : '');
+		// Set URL components
+		$this->scheme = isset($component['scheme']) ? $component['scheme'] : '';
+		$this->host = isset($component['host']) ? $component['host'] : '';
+		$this->path = isset($component['path']) ? $component['path'] : '';
+		$this->query = isset($component['query']) ? $component['query'] : '';
+		$this->fragment = isset($component['fragment']) ? $component['fragment'] : '';
 	}
 
 	/**
 	 * Returns a string representation of the URL object.
 	 *
-	 * scheme + DELIMITER_SCHEME + host + directory + file + DELIMITER_QUERY + query
+	 * scheme + DELIMITER_SCHEME + host + path + DELIMITER_QUERY + query
 	 *
 	 * @return string a string representation of the URL object.
 	 */
@@ -53,7 +52,7 @@ class URL {
 	/**
 	 * Returns the URL.
 	 *
-	 * scheme + DELIMITER_SCHEME + host + directory + file + DELIMITER_QUERY + query
+	 * scheme + DELIMITER_SCHEME + host + path + DELIMITER_QUERY + query + DELIMITER_FRAGMENT + fragment
 	 *
 	 * @return string the URL.
 	 */
@@ -62,9 +61,20 @@ class URL {
 	}
 
 	/**
+	 * Returns the URL scheme, host, path & query.
+	 *
+	 * scheme + DELIMITER_SCHEME + host + path + DELIMITER_QUERY + query
+	 *
+	 * @return string the URL.
+	 */
+	public function getURLQuery() {
+		return $this->getURLHost() . $this->getPath() . ($this->query === '' ? '' : self::DELIMITER_QUERY . $this->query);
+	}
+
+	/**
 	 * Returns the URL scheme, host & path.
 	 *
-	 * scheme + DELIMITER_SCHEME + host + directory + file
+	 * scheme + DELIMITER_SCHEME + host + path
 	 * 
 	 * @return string the URL scheme, host & path.
 	 */
@@ -80,7 +90,7 @@ class URL {
 	 * @return string the URL scheme, host & directory.
 	 */
 	public function getURLDirectory() {
-		return $this->getURLHost() . $this->directory;
+		return $this->getURLHost() . $this->getDirectory();
 	}
 
 	/**
@@ -92,28 +102,6 @@ class URL {
 	 */
 	public function getURLHost() {
 		return ($this->scheme === '' ? '' : $this->scheme . self::DELIMITER_SCHEME) . $this->host;
-	}
-
-	/**
-	 * Returns the URI.
-	 *
-	 * directory + file + DELIMITER_QUERY + query
-	 *
-	 * @return string the URI.
-	 */
-	public function getURI() {
-		return $this->getPath() . ($this->query === '' ? '' : self::DELIMITER_QUERY . $this->query);
-	}
-
-	/**
-	 * Returns the path.
-	 *
-	 * directory + file
-	 *
-	 * @return string the path.
-	 */
-	public function getPath() {
-		return $this->directory . $this->file;
 	}
 
 	/**
@@ -135,12 +123,32 @@ class URL {
 	}
 
 	/**
+	 * Returns the URI.
+	 *
+	 * path + DELIMITER_QUERY + query + DELIMITER_FRAGMENT + fragment
+	 *
+	 * @return string the URI.
+	 */
+	public function getURI() {
+		return $this->getPath() . ($this->query === '' ? '' : self::DELIMITER_QUERY . $this->query) . ($this->fragment === '' ? '' : self::DELIMITER_FRAGMENT . $this->fragment);
+	}
+
+	/**
+	 * Returns the path.
+	 *
+	 * @return string the path.
+	 */
+	public function getPath() {
+		return $this->path;
+	}
+
+	/**
 	 * Returns the directory.
 	 *
 	 * @return string the directory.
 	 */
 	public function getDirectory() {
-		return $this->directory;
+		return substr($this->path, -1) === self::DELIMITER_SEGMENT ? $this->path : dirname($this->path) . self::DELIMITER_SEGMENT;
 	}
 
 	/**
@@ -149,7 +157,7 @@ class URL {
 	 * @return string the file.
 	 */
 	public function getFile() {
-		return $this->file;
+		return substr($this->path, -1) === self::DELIMITER_SEGMENT ? '' : basename($this->path);
 	}
 
 	/**
@@ -159,6 +167,89 @@ class URL {
 	 */
 	public function getQuery() {
 		return $this->query;
+	}
+
+	/**
+	 * Returns the fragment.
+	 *
+	 * @return string the fragment.
+	 */
+	public function getFragment() {
+		return $this->fragment;
+	}
+
+	/**
+	 * Set the scheme.
+	 *
+	 * @param  string $scheme
+	 * @return void
+	 */
+	public function setScheme($scheme) {
+		$this->scheme = $scheme;
+	}
+
+	/**
+	 * Set the host.
+	 *
+	 * @param  string $host
+	 * @return void
+	 */
+	public function setHost($host) {
+		$this->host = $host;
+	}
+
+	/**
+	 * Set the path.
+	 *
+	 * @param  string $path
+	 * @return void
+	 */
+	public function setPath($path) {
+		$this->path = $path;
+	}
+
+	/**
+	 * Set the query
+	 *
+	 * @param  array   $query
+	 * @param  boolean $encode = FALSE
+	 * @return void
+	 */
+	public function buildQuery(array $query, $encode = FALSE) {
+		// Build
+		$this->query = http_build_query($query);
+
+		// Encode
+		if($encode) $this->query = htmlentities($this->query);
+	}
+
+	/**
+	 * Set the query.
+	 *
+	 * @param  string $query
+	 * @return void
+	 */
+	public function setQuery($query) {
+		$this->query = $query;
+	}
+
+	/**
+	 * Set the fragment.
+	 *
+	 * @param  string $fragment
+	 * @return void
+	 */
+	public function setFragment($fragment) {
+		$this->fragment = $fragment;
+	}
+
+	/**
+	 * Returns the URL segements
+	 *
+	 * @return array the URL segments
+	 */
+	public function getSegments() {
+		return explode(self::DELIMITER_SEGMENT, substr($directory, strlen(self::DELIMITER_SEGMENT), -strlen(self::DELIMITER_SEGMENT)));
 	}
 }
 
